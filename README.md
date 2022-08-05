210f07221f7fc327432b5ca195f09860feceedc8
jhajduk-microsoft <jhajduk@microsoft.com>
2022-08-05 13:46:58 -0700

    SSR, MSE

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ Coding the examples from the StatQuest Illustrated Guide to Machine Learning (Py
 - Exponential distributions are good for determining the likelihood of the time that passes between events.
 
 - Uniform distributions can be used to generate random numbers (e.g. between 0 and 1)
+
+- Probability distributions are types of models that allow us to give approximations as if we had an unlimited amount of data.
+
+## Models
+
+- Models, such as the equation of a line, can tell us about data we have not measured yet.
+
+- Since models are only approximations, we need to measure their quality - their distance from their predictions, using statistics
+
+- Sum of squared residuals: One way to determine the quality of a models predictions
+
+- The issue with the Sum of Squared Residuals is that it only tells us the residuals, not that a model is better or worse that another
+
+- Mean Squared Error (average of SSR) can be used to compare to models with different sized data sets
---
+++ SSR/SSR_MSE.py
@@ -0,0 +1,23 @@
+'''
+This program calculates the sum of squared residuals. This calculation is useful in determining if a model's predictions are trustworthy.
+Residual = Observed - Predicted
+SSR = sum(Observed - Predicted)^2
+The sum of squared residuals is the sum of all observations of the squared difference between the observed and predicted values
+'''
+
+def sumOfSquaredResiduals(obs, pres, base_ssr, total_length):
+    for i in range(total_length):
+        base_ssr += (obs[i] - pres[i])**2
+    return base_ssr
+
+observations = list(map(float, input("Please enter the observations, separated by a single space \n").split()))
+predictions = list(map(float, input("Please enter the predictions, separated by a single space \n").split()))
+assert len(observations) == len(predictions), "The number observations and predictions is different. Please enter the the same number of observations and predictions"
+total_len = len(observations)
+SSR = 0
+
+calc_ssr = sumOfSquaredResiduals(observations, predictions, SSR, total_len)
+mse = calc_ssr/observations
+
+print("The Sum of Squared Residuals is: ", calc_ssr)
+print("The Mean Squared Error is: ", mse)
