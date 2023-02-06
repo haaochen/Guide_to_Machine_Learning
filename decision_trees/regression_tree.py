f1fb9b88ab14db0f928ec34d35236e377b9acd59
jenniferamhajduk <jenalimor1@gmail.com>
2023-02-05 18:21:55 -0800

    finished regression tree

--- decision_trees/regression_tree.py
+++ decision_trees/regression_tree.py
@@ -36,18 +36,16 @@ def regression_tree(de_list):
 
 ssr_list = regression_tree(doses_effectiveness)
 values_to_the_left = doses_effectiveness.index(doses_effectiveness[ssr_list.index(min(ssr_list)) + 1])
-print("Since there are {0} values to the left of the split value. This might lead to overfitting. We will not split further as the minimum number of values to continue on either side is {1}".format(values_to_the_left, split_min))
+print("Since there are {0} values to the left of the split value. This might lead to overfitting if we apply regression to those values. We will not split those values further as the minimum number of values to continue on either side is {1}".format(values_to_the_left, split_min))
 low_average_effectiveness = 0
 for i in range(values_to_the_left):
     low_average_effectiveness += doses_effectiveness[i][1]
 low_average_effectiveness = low_average_effectiveness/values_to_the_left
-
 print("We will add a leaf to the left of the split value, composed of the average effectiveness of the remaining values to the left of the split value. The average effectiveness for the values to the left of the split value is: {0}%".format(low_average_effectiveness))
-
 print("Lets continue to the right side of the split value and including the split value...")
 doses_effectiveness = doses_effectiveness[values_to_the_left:]
 print("Let's just take those values into a new list: {0}".format(doses_effectiveness))
-
+print('-'*25)
 ssr_list = regression_tree(doses_effectiveness)
 split_value = doses_effectiveness[ssr_list.index(min(ssr_list)) + 1]
 print("The split value is: {0}".format(split_value))
@@ -57,7 +55,45 @@ for i in range(-1,values_to_the_right + 1,-1):
     high_average_effectiveness += doses_effectiveness[i][1]
 high_average_effectiveness = high_average_effectiveness/values_to_the_right
 print("Since there are {0} values to the right, which is less than our split minimum of {1}, then we calculate the average effectiveness for those values including the split value and make a leaf on the left of our split value because we want to test for values more than or equal to the split value: {2}%".format(values_to_the_right, split_min, high_average_effectiveness))
-print("Lets remove those values from the list to clean things up...")
+print("Lets remove those values from the list to clean things up...and create yet another list on which to operate, which include the remaining values")
 doses_effectiveness = doses_effectiveness[:-values_to_the_right]
+print("The remaining list is: {0}".format(doses_effectiveness))
+print('-'*25)
+ssr_list = regression_tree(doses_effectiveness)
+split_value = doses_effectiveness[ssr_list.index(min(ssr_list)) + 1]
+print("The split value is: {0}".format(split_value))
+values_to_the_right = len(doses_effectiveness) - doses_effectiveness.index(doses_effectiveness[ssr_list.index(min(ssr_list)) + 1]) - 1
+high_average_effectiveness = 0
+for i in range(doses_effectiveness.index(split_value) + 1,len(doses_effectiveness)):
+    high_average_effectiveness += doses_effectiveness[i][1]
+high_average_effectiveness = high_average_effectiveness/values_to_the_right
 
-
+print("There are only three values on the right side of the split value {0}: {1} '\n'So we average the effectiveness and make that average a leaf: {2}%".format(split_value,doses_effectiveness[doses_effectiveness.index(split_value) + 1:], high_average_effectiveness))
+doses_effectiveness = [x for x in doses_effectiveness[0:len(doses_effectiveness) - values_to_the_right]]
+print("The remaining items in the list are: {0}".format(doses_effectiveness))
+print('-'*25)
+print("Let's perform regression on the remaining list...")
+ssr_list = regression_tree(doses_effectiveness)
+split_value = doses_effectiveness[ssr_list.index(min(ssr_list)) + 1]
+print("The split value is: {0}".format(split_value))
+values_to_the_right = len(doses_effectiveness) - doses_effectiveness.index(doses_effectiveness[ssr_list.index(min(ssr_list)) + 1]) - 1
+high_average_effectiveness = 0
+for i in range(doses_effectiveness.index(split_value) + 1,len(doses_effectiveness)):
+    high_average_effectiveness += doses_effectiveness[i][1]
+high_average_effectiveness = high_average_effectiveness/values_to_the_right
+print("There is only one value on the right side of the split value {0}: {1} '\n'So we average the effectiveness and make that average a leaf: {2}%".format(split_value,doses_effectiveness[doses_effectiveness.index(split_value) + 1:], high_average_effectiveness))
+doses_effectiveness = [x for x in doses_effectiveness[0:len(doses_effectiveness) - values_to_the_right]]
+print("The remaining items in the list are: {0}".format(doses_effectiveness))
+print('-'*25)
+print("Let's perform regression on the remaining list...")
+ssr_list = regression_tree(doses_effectiveness)
+split_value = doses_effectiveness[ssr_list.index(min(ssr_list)) + 1]
+print("The split value is: {0}".format(split_value))
+values_to_the_right = len(doses_effectiveness) - doses_effectiveness.index(doses_effectiveness[ssr_list.index(min(ssr_list)) + 1]) - 1
+doses_effectiveness = [x for x in doses_effectiveness[0:len(doses_effectiveness) - 1]]
+final_avg_sum = 0
+for i in range(len(doses_effectiveness)):
+    final_avg_sum += doses_effectiveness[i][1]
+average_effectiveness = final_avg_sum/len(doses_effectiveness)
+print("Since there are no values to the right of the split value, we prune this value and this remaining values are: {0}".format(doses_effectiveness))
+print("Since there are less than the minimum number of values needed, we end our regression here. We can see that values for doeses less than 14 and more than 28 have been pruned so values for doses between 14 and 28 provide the highest effectiveness. In a perfect example, we would have been left with only 100 percent effective doeses but we wanted to make up these values for experiments sake. The average effectiveness of these doese within the testing threshold is: {0}%".format(average_effectiveness))
